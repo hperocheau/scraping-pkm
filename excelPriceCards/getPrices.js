f179888ae99f175b18b7377b831768edf646007b
admin <hperocheau@gmail.com>
2024-01-28 19:17:44 +0100

    getPrices.js update -> use already created sheet and update it by creating a F column to write the prices. If the cell are empty or with 'no data test' -> go find the price average. If cell value is 'no data found' -> no card up to sell so keep it

--- excelPriceCards/getPrices.js
+++ excelPriceCards/getPrices.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const ExcelJS = require('exceljs');
+const moment = require('moment');
 const fs = require('fs');
 
 (async () => {
@@ -11,83 +12,65 @@ const fs = require('fs');
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
 
   const workbook = new ExcelJS.Workbook();
-  const xlsxFilePath = './cartes_url.xlsx';
+  const xlsxFilePath = './cartes.xlsx';
 
   // Vérifier si le fichier Excel existe
   if (fs.existsSync(xlsxFilePath)) {
     await workbook.xlsx.readFile(xlsxFilePath);
 
     // Générer un nom de feuille valide avec la date d'aujourd'hui
-    const today = new Date().toLocaleDateString('fr-FR', { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/\//g, '-');
-    const existingSheet = workbook.getWorksheet(today);
+    const today = moment().format("DD_MM_YYYY");
+    const worksheet = workbook.getWorksheet(today);
 
-    let worksheet2;
-    if (existingSheet) {
-      // Si la feuille existe, utilisez-la
-      worksheet2 = existingSheet;
-    } else {
-      // Sinon, créez une nouvelle feuille avec la date d'aujourd'hui comme nom
-      worksheet2 = workbook.addWorksheet(today);
+    if (worksheet) {
+      // Set the header for column F
+      worksheet.getCell('F1').value = 'Prix moyen';
 
-      // Copier la Feuil1 dans la nouvelle feuille
-      workbook.getWorksheet('Feuil1').eachRow((row, rowNumber) => {
-        const newRow = worksheet2.getRow(rowNumber);
-        row.eachCell((cell, colNumber) => {
-          newRow.getCell(colNumber).value = cell.value;
-        });
-      });
-    }
+      let cardType;
+      let rowIndex;
 
-    let cardType;
-    let rowIndex;
-
-    for (let i = 2; i <= worksheet2.lastRow.number; i++) {
-      rowIndex = i;
-      const urlCell = worksheet2.getCell(`E${i}`);
-      const langCell = worksheet2.getCell(`D${i}`);
-      const cardTypeCell = worksheet2.getCell(`A${i}`);
-      const url = urlCell.value;
-      const language = langCell.value && langCell.value.toString().toLowerCase();
-      cardType = cardTypeCell.value && cardTypeCell.value.toString().toLowerCase();
-
-      console.log(`Lecture de l'URL depuis la cellule E${i}: ${url} avec la langue: ${language}`);
-      await page.waitForTimeout(1500);
-
-      // Vérifier si la cellule F contient "no data test"
-      const priceCell = worksheet2.getCell(`F${i}`);
-      if (priceCell.value === 'no data test') {
-        if (url) {
-          console.log(`Traitement de l'URL: ${url}`);
-          let finalURL = url;
-
-          // Personnaliser l'URL en fonction de la langue
-          if (language === 'jp' || language === 'japonais' || language === 'jap') {
-            finalURL += '?language=7&minCondition=2&isSigned=N&isPlayset=N&isAltered=N';
-          } else if (language === 'fr' || language === 'français' || language === 'francais') {
-            finalURL += '?language=2&minCondition=2&isSigned=N&isPlayset=N&isAltered=N';
-          }
-
-          // Mise à jour de l'URL dans le fichier Excel
-          urlCell.value = finalURL;
-
-          await page.goto(finalURL);
+      for (let i = 2; i <= worksheet.lastRow.number; i++) {
+        try {
+          rowIndex = i;
+          const urlCell = worksheet.getCell(`E${i}`);
+          const cardTypeCell = worksheet.getCell(`A${i}`);
+          const url = urlCell.value;
+          cardType = cardTypeCell.value && cardTypeCell.value.toString().toLowerCase();
+      
+          console.log(`Lecture de l'URL depuis la cellule E${i}: ${url}`);
           await page.waitForTimeout(1500);
-
-          // Vérifier la présence de la balise indiquant l'absence de résultats
-          const noResultsElement = await page.$('.noResults.text-center.h3.text-muted.py-5');
-
-          if (noResultsElement) {
-            // Balise présente, aucune donnée n'est disponible
-            console.log(`Aucune donnée disponible pour la cellule E${i}`);
-            priceCell.value = 'no data found';
-          } else {
-            // Balise non trouvée, il y a des données ou la page est HS
-            // Récupérer les valeurs des trois premières balises span
-            const averagePrice = await page.evaluate((cardType, rowIndex) => {
-              const articles = document.querySelectorAll('[id^="articleRow"]');
-              const prices = [];
-
-              articles.forEach((elem) => {
+      
+          // Vérifier si la cellule F contient "no data test"
+          const priceCell = worksheet.getCell(`F${i}`);
+          const priceCellValue = priceCell.value !== null ? priceCell.value.toString().trim().toLowerCase() : '';
+      
+          if (priceCellValue === 'no data test' || priceCellValue === '') {
+            console.log(`Traitement de l'URL: ${url}`);
+            let finalURL = url;
+      
+            await page.goto(finalURL);
+            await page.waitForTimeout(1500);
+      
+            // Vérifier la présence de la balise indiquant l'absence de résultats
+            const noResultsElement = await page.$('.noResults.text-center.h3.text-muted.py-5');
+      
+            if (noResultsElement) {
+              // Balise présente, aucune donnée n'est disponible
+              console.log(`Aucune donnée disponible pour la cellule E${i}`);
+              priceCell.value = 'no data found';
+            } else {
+              // Balise non trouvée, il y a des données ou la page est HS
+              // Récupérer les valeurs des trois premières balises span
+              const averagePrice = await page.evaluate((cardType, rowIndex) => {
+                console.log(`Début de l'évaluation pour la cellule E${rowIndex}`);
+              
+                try {
+                  const articles = document.querySelectorAll('[id^="articleRow"]');
+                  const prices = [];
+              
+                  articles.forEach((elem, index) => {
+                    console.log(`Traitement de l'article ${index + 1}`);
+                  
                 const formatPriceToFloat = (text) => parseFloat(text.trim().replace(".", "").replace(',', '.').trim().match(/\d+(?:\.\d+)?/g))
                 let comment = elem.querySelector('.product-comments')?.textContent;
                 const price = elem.querySelector(".price-container").textContent;
@@ -106,28 +89,48 @@ const fs = require('fs');
                 if (prices.length === 0) {
                   return; // Aucun prix trouvé
                 }
-              });
-
-              const numberOfCards = prices.length > 2 ? 3 : prices.length;
-              const totalPrice = prices.slice(0, numberOfCards).reduce((acc, price) => acc + price, 0);
-
-              return totalPrice / numberOfCards;
+                });
+      
+                if (prices.length === 0) {
+                  console.log(`Aucun prix trouvé pour la cellule E${rowIndex}`);
+                  return null; // Aucun prix trouvé
+                }
+            
+                const numberOfCards = prices.length > 2 ? 3 : prices.length;
+                const totalPrice = prices.slice(0, numberOfCards).reduce((acc, price) => acc + price, 0);
+            
+                console.log(`Fin de l'évaluation pour la cellule E${rowIndex}`);
+                return totalPrice / numberOfCards;
+              } catch (error) {
+                console.error(`Erreur pendant l'évaluation de la page pour la cellule E${rowIndex}: ${error.message}`);
+                return null;
+              }
             }, cardType, rowIndex);
-
-            await page.waitForTimeout(1500);
-
-            let priceValue = averagePrice !== null ? averagePrice.toFixed(2) : 'no data found';
-            priceValue = isNaN(priceValue) ? 'no data test' : priceValue;
-            priceCell.value = priceValue;
-            console.log(`Prix moyen ajouté à la cellule F${i} de la feuille Feuil2: ${priceValue}`);
+      
+              if (averagePrice !== null) {
+                await page.waitForTimeout(1500);
+      
+                let priceValue = averagePrice.toFixed(2);
+                priceCell.value = priceValue;
+                console.log(`Prix moyen ajouté à la cellule F${i} de la feuille ${today}: ${priceCell}`);
+              } else {
+                console.log(`Aucun prix moyen trouvé pour la cellule E${i}`);
+              }
+            }
+          } else {
+            console.log(`La cellule F${i} n'a pas besoin d'être mise à jour.`);
           }
+        } catch (error) {
+          console.error(`Erreur lors du traitement de la cellule E${i}: ${error.message}`);
         }
       }
-    }
 
-    // Mise à jour du fichier Excel
-    await workbook.xlsx.writeFile(xlsxFilePath);
-    console.log('Fichier Excel mis à jour avec succès.');
+      // Mise à jour du fichier Excel
+      await workbook.xlsx.writeFile(xlsxFilePath);
+      console.log(`Fichier Excel mis à jour avec succès. Feuille utilisée : ${today}`);
+    } else {
+      console.error(`La feuille avec le nom ${today} n'existe pas.`);
+    }
   } else {
     console.error('Le fichier Excel n\'existe pas.');
   }
